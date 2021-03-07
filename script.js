9c40aa2cdb5fcc117bd697fd70cf45457d973e51
MadhaneeswaranP <78028332+MadhaneeswaranP@users.noreply.github.com>
2021-03-07 12:18:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// //1.Create a request variable
+// var request =new XMLHttpRequest();
+// //2.Create a connection.
+// //Open has three parameter(get method,url,boolean)
+// request.open('GET','https://restcountries.eu/rest/v2/all',true);
+// //3.Send the request
+// request.send();
+// //4.Process and Load the response
+// request.onload = function(){  //function name not necessary coz attached to event
+//   var data=JSON.parse(this.response);
+//   let countriesHTML="";
+//   for(let i=0;i<5;i++){
+//     countriesHTML += ` 
+//     <div class="card" style="width: 18em;">
+//         <div class="card card-header" id="id-name" style="background-color: black; color: blanchedalmond; text-align: center;">${data[i].name}</div>
+//         <img class="card-img-top" id="img-id" src="${data[i].flag}" style="padding: 10px;background-image: linear-gradient(90deg,#cede64,#6e7541);">
+//         <div class="card-body" style="background-image: linear-gradient(90deg,#cede64,#6e7541);text-align: center;">
+//             <p class="card-text" id="p-id-capital">${data[i].capital}</p>
+//             <p class="card-text" id="p-id-population">${data[i].population}</p>
+//             <p class="card-text" id="p-id-region">${data[i].region}</p>
+//             <p class="card-text" id="p-id-country-code">${data[i].alpha3Code}</p>
+//             <button class="btn btn-dark" style="background-color: transparent;">Click for weather</button>
+//         </div>
+//     </div>
+//     `
+//   }
+// }
 var data = fetch("https://restcountries.eu/rest/v2/all");
 
 data.then(function(response){
@@ -16,7 +43,7 @@ data.then(function(response){
                 <p class="card-text" id="p-id-population">Population:${data[i].population}</p>
                 <p class="card-text" id="p-id-region">Region:${data[i].region}</p>
                 <p class="card-text" id="p-id-country-code">Country Code:${data[i].alpha3Code}</p>
-                <button class="btn btn-dark weather-btn" style="background-color: transparent;" countryName=${data[i].latlng} onclick="getLatLong(this)">Click for weather</button>
+                <button class="btn btn-dark weather-btn" style="background-color: transparent;" countryName=${data[i].name} onclick="getLatLong(this)">Click for weather</button>
             </div>
         </div>
         `
@@ -28,7 +55,7 @@ document.querySelector(".container").innerHTML = countriesHTML;
 
 function getLatLong(ele){
     var ctName = ele.getAttribute('countryName');
-   var data = fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${ctName[0]}&lon=${ctName[1]}&appid=fa2fc8ee8fa3b9d189f1d15326523568`);
+    var data = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ctName}&appid=fa2fc8ee8fa3b9d189f1d15326523568`);
     
     data.then(function(response){
         return response.json();
